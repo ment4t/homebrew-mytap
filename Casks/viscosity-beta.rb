b7ec3b92a0f08a68b5acd5a8967957b7a311be72
ment4t <gregd@barmail.ch>
2014-05-16 18:33:19 +0100

    2nd commit

---
+++ Casks/viscosity-beta.rb
@@ -0,0 +1,7 @@
+class ViscosityBeta < Cask
+  url 'http://www.sparklabs.com/downloads/beta/Viscosity1.4.9b4.zip'
+  homepage 'http://www.sparklabs.com/viscosity/'
+  version '1.4.9b3'
+  sha256 'de71a67461acdfa75fa79cbb30f3a8539c5d4b907bffb04aeb899df80b3d3849'
+  link 'Viscosity.app'
+end
