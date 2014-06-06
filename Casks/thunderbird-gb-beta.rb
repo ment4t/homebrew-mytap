b24dfac7c175b4fe7ce73d239c8d8934ad55ed70
ment4t <gregd@barmail.ch>
2014-06-06 12:40:28 +0100

    another commit

--- Casks/thunderbird-gb-beta.rb
+++ Casks/thunderbird-gb-beta.rb
@@ -1,7 +1,7 @@
 class ThunderbirdGbBeta < Cask
   url 'https://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/latest-beta/mac/en-GB/Thunderbird%2030.0b1.dmg'
   homepage 'http://www.mozilla.com/thunderbird/'
-  version '30.0b1.dmg'
+  version '30.0b1'
   sha256 'f6262ce3c044ec014dc83965385f8f61028c178490c260023eb016f7eb0f6317'
   link 'Thunderbird.app'
 end
---
+++ Casks/thunderbird-us-alpha.rb
@@ -0,0 +1,7 @@
+class ThunderbirdUsAlpha < Cask
+  url 'https://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly/latest-earlybird/thunderbird-31.0a2.en-US.mac.dmg'
+  homepage 'http://www.mozilla.com/thunderbird/'
+  version '31.0a2'
+  sha256 '0ab1398a21aaa1835ac175dfd2d91ed56cf9b7e85a8d3caae445c0573d7ce5fb'
+  link 'Earlybird.app'
+end
